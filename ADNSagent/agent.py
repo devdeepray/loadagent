05e1ac9d9217830dd394036df93d08c728dcf60b
Devdeep Ray <devdeepray@gmail.com>
2015-10-14 10:43:47 +0530

    Made some fixes to the ADNS agent

--- ADNSagent/agent.py
+++ ADNSagent/agent.py
@@ -15,8 +15,9 @@ INTERVAL = float(config['GENERAL']['interval'])
 TIMEOUT = float(config['GENERAL']['interval'])
 SQLU = config['GENERAL']['sqlu']
 SQLP = config['GENERAL']['sqlp']
+SQLD = config['GENERAL']['sqld']
 
-DB = MySQLDatabase('pdns', user=SQLU, passwd=SQLP)
+DB = MySQLDatabase(SQLD, user=SQLU, passwd=SQLP)
 
 class DCLoadRecords(peewee.Model):
     dcip = peewee.CharField(primary_key=True)
@@ -27,29 +28,38 @@ class DCLoadRecords(peewee.Model):
     class Meta:
         database = DB
 
-#DCLoadRecords.create_table()
-#le = DCLoadRecords(dcip='dc1.ipaddr', networkload=22.221, serveron=True, timestamp=datetime.datetime.now(), volatility=1)
-#DCLoadRecords.create(**le)
-#le.save(force_insert=True)
-#le = DCLoadRecords(dcip='dc2.ipaddr', networkload=22.221, serveron=True, timestamp=datetime.datetime.now(), volatility=1)
-#le.save()
+# Set up table and entries for servers if they don't exist.
+try:
+    DCLoadRecords.drop_table()
+except Exception:
+    print('Table does not exist, creating new')
 
+DCLoadRecords.create_table()
+
+for ip in config['DC_IP']:
+    le = DCLoadRecords(dcip=config['DC_IP'][ip], networkload=0, serveron=False, timestamp=datetime.datetime.now(), volatility=1)
+    le.save(force_insert=True)
+
+# Thread for updating the table periodically by polling the DC.
 def updaterThread(ip, pt):
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             s.connect((ip, int(pt)))
+            print('Connected to ' + ip + ' on port ' + pt)
         except Exception:
+            print('Could not connect to ' + ip + ' on port ' + pt)
             time.sleep(1)
             s.close()
             continue
         while True:
-            dat = DCLoadRecords(dcip='dc1.ipaddr')
+            dat = DCLoadRecords(dcip=ip)
             s.send(bytes('a', "utf-8"))
             try:
                 data = s.recv(128)
                 dat.serveron = True
             except Exception:
+                print('Connection to ' + ip + ', ' + pt + ' timed out.')
                 dat.serveron = False
                 dat.timestamp = datetime.datetime.now()
                 dat.save()
